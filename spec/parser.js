df33f4e4e5d2ffce879e8031536cd93614da52f6
Ruben Taelman <ruben.taelman@ugent.be>
2020-04-03 17:47:40 +0200

    Add missing spec folder

---
+++ spec/parser.js
@@ -0,0 +1,18 @@
+const rdfParser = require("..").default;
+const { ErrorSkipped } = require('rdf-test-suite');
+
+module.exports = {
+  parse: function (data, baseIRI, options) {
+    if (options.processingMode && (options.processingMode !== '1.0' && options.processingMode !== '1.1')) {
+      return Promise.reject(
+        new ErrorSkipped(`Test with processing mode ${options.processingMode} was skipped, only 1.0 is supported.`));
+    }
+    if (options.specVersion && options.specVersion !== '1.1') {
+      return Promise.reject(
+        new ErrorSkipped(`Test with spec version ${options.specVersion} was skipped, only 1.1 is supported.`));
+    }
+    return require('arrayify-stream')(rdfParser.parse(require('streamify-string')(data), Object.assign({
+        baseIRI,
+      }, options)));
+  },
+};
